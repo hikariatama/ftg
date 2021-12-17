6b65c6c4dfbee7b6959a103f47e5620e963a0344
Dan <innocoffee@ya.ru>
2021-12-17 17:56:57 +0300

    Add support of -c (command) option to Keyword

--- keyword.py
+++ keyword.py
@@ -11,7 +11,7 @@
 # <3 pic: https://img.icons8.com/fluency/48/000000/macbook-chat.png
 # <3 desc: Отвечать на определенные сообщения заданной фразой
 
-from .. import loader, utils
+from .. import loader, utils, main
 import logging
 import re
 
@@ -24,13 +24,13 @@ class KeywordMod(loader.Module):
         'name': 'Keyword',
         'args': '🦊 <b>Args are incorrect</b>',
         'kw_404': '🦊 <b>Keyword "{}" not found in db</b>', 
-        'kw_added': '🦊 <b>New keyword:\nTrigger: {}\nMessage: {}\n{}{}{}{}</b>',
+        'kw_added': '🦊 <b>New keyword:\nTrigger: {}\nMessage: {}\n{}{}{}{}{}</b>',
         'kw_removed': '🦊 <b>Keyword "{}" removed</b>', 
         'kwbl_list': '🦊 <b>Blacklisted chats:</b>\n{}', 
         'bl_added': '🦊 <b>This chat is now blacklisted for Keywords</b>',
         'bl_removed': '🦊 <b>This chat is now whitelisted for Keywords</b>',
         'sent': '🦊 <b>[Keywords]: Sent message to {}, triggered by {}:\n{}</b>',
-        'kwords': '🦊 <b>Current keywords:\n{}</b>'
+        'kwords': '🦊 <b>Current keywords:\n</b>{}'
     }
 
     async def client_ready(self, client, db):
@@ -42,7 +42,7 @@ class KeywordMod(loader.Module):
     async def kwordcmd(self, message):
         """<keyword | could be in quotes | & for multiple words that should be in msg> <message | empty to remove keyword> [-r for full match] [-m for autoreading msg] [-l to log in pm] [-e for regular expressions]"""
         args = utils.get_args_raw(message)
-        kw, ph, restrict, ar, l, e = "", "", False, False, False, False
+        kw, ph, restrict, ar, l, e, c = "", "", False, False, False, False, False
         if '-r' in args:
             restrict = True
             args = args.replace(' -r', '').replace('-r', '')
@@ -58,6 +58,11 @@ class KeywordMod(loader.Module):
         if '-e' in args:
             e = True
             args = args.replace(' -e', '').replace('-e', '')
+
+        if '-c' in args:
+            c = True
+            args = args.replace(' -c', '').replace('-c', '')
+
 
         if args[0] == "'":
             kw = args[1:args.find("'", 1)]
@@ -83,16 +88,16 @@ class KeywordMod(loader.Module):
         else:
             ph = ph.strip()
             kw = kw.strip()
-            self.keywords[kw] = [ph, restrict, ar, l, e]
+            self.keywords[kw] = ['🤖 ' + ph, restrict, ar, l, e, c]
             self.db.set('Keyword', 'keywords', self.keywords)
-            return await utils.answer(message, self.strings('kw_added').format(kw, ph, ('Restrict: yes\n' if restrict else ''), ('Auto-read: yes\n' if ar else ''), ('Log: yes' if l else ''), ('Regex: yes' if e else '')))
+            return await utils.answer(message, self.strings('kw_added').format(kw, utils.escape_html(ph), ('Restrict: yes\n' if restrict else ''), ('Auto-read: yes\n' if ar else ''), ('Log: yes' if l else ''), ('Regex: yes' if e else ''), ('Command: yes' if c else '')))
 
 
     async def kwordscmd(self, message):
         """List current kwords"""
         res = ""
         for kw, ph in self.keywords.items():
-            res += kw + '\nMessage: ' + ph[0] + '\n' + ('Restrict: yes\n' if ph[1] else '') + ('Auto-read: yes\n' if ph[2] else '') + ('Log: yes' if ph[3] else '') + ('Regex: yes' if len(ph) > 4 and ph[4] else '')
+            res += '<code>' + kw + '</code>\n<b>Message: ' + utils.escape_html(ph[0]) + '\n' + ('Restrict: yes\n' if ph[1] else '') + ('Auto-read: yes\n' if ph[2] else '') + ('Log: yes' if ph[3] else '') + ('Regex: yes' if len(ph) > 4 and ph[4] else '') + ('Command: yes' if len(ph) > 5 and ph[5] else '') + '</b>'
             if res[-1] != '\n':
                 res += '\n'
 
@@ -143,7 +148,7 @@ class KeywordMod(loader.Module):
             cid = utils.get_chat_id(message)
             if cid in self.bl: return
 
-            for kw, ph in self.keywords.items():
+            for kw, ph in self.keywords.copy().items():
                 if len(ph) > 4 and ph[4]:
                     try:
                         if not re.match(kw, message.raw_text): continue
@@ -154,16 +159,21 @@ class KeywordMod(loader.Module):
                     kws = [_.strip() for _ in ([kw] if '&' not in kw else kw.split('&'))]
                     trigger = False
                     for k in kws:
-                        if k in message.text:
+                        if k.lower() in message.text.lower():
                             trigger = True
                             if not ph[1]:
                                 break
-                        elif k not in message.text and ph[1]:
+                        elif k.lower() not in message.text.lower() and ph[1]:
                             trigger = False
                             break
 
                     if not trigger:
                         continue
+
+                offset = 2
+
+                if len(ph) > 5 and ph[5] and ph[0][offset:].startswith(utils.escape_html((self.db.get(main.__name__, "command_prefix", False) or ".")[0])):
+                    offset += 1
 
                 if ph[2]:
                     await self.client.send_read_acknowledge(cid, clear_mentions=True)
@@ -175,7 +185,19 @@ class KeywordMod(loader.Module):
                         ch = (chat.title if getattr(message, 'title', None) is not None else '')
                     await self.client.send_message('me', self.strings('sent').format(ch, kw, ph[0]))
 
-                return await utils.answer(message, ph[0])
+                ms = await utils.answer(message, ph[0])
+
+                try:
+                    ms = ms[0]
+                except: pass
+
+                ms.text = ph[0][2:]
+
+                if len(ph) > 5 and ph[5]:
+                    cmd = ph[0][offset:].split()[0]
+                    if cmd in self.allmodules.commands:
+                        await self.allmodules.commands[cmd](ms)
+
         except Exception as e:
             logger.exception(e)
             pass
